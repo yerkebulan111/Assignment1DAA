df6c062bb1064c10fbc4b10cd54654506bded5d3
yerkebulan <abdisattarerkebulan@gmail.com>
2025-10-01 13:22:12 +0500

    refactor(util): partition, swap, shuffle, guards

--- src/main/java/com/company/sorts/quicksort/QuickSortRandomPivot.java
+++ src/main/java/com/company/sorts/quicksort/QuickSortRandomPivot.java
@@ -1,11 +1,9 @@
 package com.company.sorts.quicksort;
 
 import com.company.metrics.Metrics;
-
-import java.util.Random;
+import com.company.util.SortUtils;
 
 public class QuickSortRandomPivot {
-    private static final Random rand = new Random();
 
     public static void sort(int[] arr, Metrics metrics) {
         quickSort(arr, 0, arr.length - 1, metrics);
@@ -16,37 +14,11 @@ public class QuickSortRandomPivot {
             metrics.incrementRecursiveCalls();
             metrics.enterDepth();
 
-            int pivotIndex = partition(arr, low, high, metrics);
+            int pivotIndex = SortUtils.partition(arr, low, high, metrics);
             quickSort(arr, low, pivotIndex - 1, metrics);
             quickSort(arr, pivotIndex + 1, high, metrics);
 
             metrics.exitDepth();
-        }
-    }
-
-    private static int partition(int[] arr, int low, int high, Metrics metrics) {
-        int pivotIndex = rand.nextInt(high - low + 1) + low;
-        swap(arr, pivotIndex, high, metrics);
-        int pivot = arr[high];
-
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            metrics.incrementComparisons();
-            if (arr[j] <= pivot) {
-                i++;
-                swap(arr, i, j, metrics);
-            }
-        }
-        swap(arr, i + 1, high, metrics);
-        return i + 1;
-    }
-
-    private static void swap(int[] arr, int i, int j, Metrics metrics) {
-        if (i != j) {
-            metrics.incrementSwaps();
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
         }
     }
 }
---
+++ src/main/java/com/company/util/SortUtils.java
@@ -0,0 +1,57 @@
+package com.company.util;
+
+import com.company.metrics.Metrics;
+import java.util.Random;
+
+public class SortUtils {
+    private static final Random rand = new Random();
+
+
+    public static int partition(int[] arr, int low, int high, Metrics metrics) {
+        int pivotIndex = rand.nextInt(high - low + 1) + low;
+        swap(arr, pivotIndex, high, metrics);
+
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            metrics.incrementComparisons();
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j, metrics);
+            }
+        }
+        swap(arr, i + 1, high, metrics);
+        return i + 1;
+    }
+
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j, Metrics metrics) {
+        if (i != j) {
+            metrics.incrementSwaps();
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+
+    public static void shuffle(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            swap(arr, i, j);
+        }
+    }
+
+
+    public static boolean validRange(int low, int high) {
+        return low < high;
+    }
+}
